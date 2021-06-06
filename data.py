7d85b59fbd9664f321d96567d7f7043f6d7f567b
Mourad Hegazy <mouradhegazy95@gmail.com>
2021-06-07 00:03:43 +0200

    Delete data.py

--- data.py
+++
@@ -1,4 +0,0 @@
-import pymongo
-client = pymongo.MongoClient(
-    "mongodb+srv://Mourad:Hegazy95@mourad.6r1yb.mongodb.net/test?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs='CERT_NONE')
-db = client.get_database('linkShorter')
