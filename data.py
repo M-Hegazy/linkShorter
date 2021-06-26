69dd8dd10a163bff172b397ce85353bb92f5768a
Mourad Hegazy <mouradhegazy95@gmail.com>
2021-06-26 13:50:10 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
 import pymongo
 client = pymongo.MongoClient(
-    "mongodb+srv://Mourad:Hegazy95@mourad.6r1yb.mongodb.net/test?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs='CERT_NONE')
+    "mongnoDB_connection_link", ssl=True, ssl_cert_reqs='CERT_NONE')
 db = client.get_database('linkShorter')
